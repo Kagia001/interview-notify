b3ee60c5680667e42ad15640849fd9e4b04deb06
Karl Ragnar Giese <karl@giese.no>
2026-07-18 21:52:26 +0200

    Bump version

--- interview_notify.py
+++ interview_notify.py
@@ -8,7 +8,7 @@ from file_read_backwards import FileReadBackwards
 from hashlib import sha256
 from urllib.parse import urljoin
 
-VERSION = '1.3.0'
+VERSION = '1.3.1'
 default_server = 'https://ntfy.sh/'
 ACTIVE_WINDOW = 600 # seconds: also watch logs modified this close to the newest one
 position_lock = threading.Lock()
